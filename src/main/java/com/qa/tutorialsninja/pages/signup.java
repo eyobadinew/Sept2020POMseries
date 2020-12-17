5da3f77802e176b0d5bf17f4514b3e7857bace15
eyob <eyobadinew@gmail.com>
2020-12-16 16:40:29 -0800

    My git branching test

---
+++ src/main/java/com/qa/tutorialsninja/pages/signup.java
@@ -0,0 +1,8 @@
+package com.qa.tutorialsninja.pages;
+
+public class signup {
+	public void gitBranchingtest() {
+		System.out.println("this class is formed to test my git branching concept ");
+	}
+
+}
